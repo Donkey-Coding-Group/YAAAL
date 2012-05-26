219aa6bc40e718d4dcc94bd30516601714393000
Niklas Rosenstein <rosensteinniklas@googlemail.com>
2012-05-26 18:27:32 +0200

    Implemented Basic Server. Still bugs etc.

--- PyServer/__init__.py
+++ PyServer/__init__.py
@@ -1,1 +1,16 @@
 # coding: utf-8
+# author: Niklas Rosenstein <rosensteinniklas@googlemail.com>
+""" YAAAL - Python Server for YAAAAL
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ """
+
+import server
+
+def main():
+    host = 'localhost'
+    port = 6150
+
+    httpd = server.YAAALServer(host, port)
+    httpd.handle_request()
+
+main()
+
