126290e204a5f75d4dda0f97426fa2b290c6ac99
Niklas Rosenstein <rosensteinniklas@googlemail.com>
2012-05-27 09:45:08 +0200

    A copy of the yaaal-server and config.py that should contain the native code without implemenation of the API the YAAAL Server should provide.

---
+++ python/yaaal-server/original/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding: utf-8
+# author: Niklas Rosenstein <rosensteinniklas@googlemail.com>
+
+# HANDLERS - Specify the handlers as a dictionary, while the keys are
+# regular-epressions that will be matched against the request URI, and the
+# values are callable objects accepting 4 arguments.
+HANDLERS = {
+}
+
+# HANDLE_404 - Give a function to be able to handle requests that do not match
+# with any of the patterns above.
+HANDLE_404 = None
+
+# HANDLE_EXCEPTION - Give a function to be able to handle exceptions that might
+# occure within any of the above handlers.
+HANDLE_EXCEPTION = None
+
+
